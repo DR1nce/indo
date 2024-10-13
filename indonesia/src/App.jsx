1e3bfbbd8905ca50758c20a7d420a2bf7e0adfa9
DR1nce <tanyaanton2010@gmail.com>
2024-10-13 10:01:07 +0300

    start

---
+++ indonesia/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section className="h-screen bg-green-400 ">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic recusandae laudantium nobis dolor, ea ipsam magnam dolorem illum? Quae, quidem laboriosam eius distinctio maxime aperiam sunt! Ipsa illum facere vel.</p>
+      </section>
+    </div>
+  ); 
+  
+}
+
+export default App
